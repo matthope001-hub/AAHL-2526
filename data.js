64402d316e1703a01f29c480210984dde8ddac34
matthope001-hub <matt.hope001@gmail.com>
2026-09-03 11:33:02 -0400

    Update data.js

--- data.js
+++ data.js
@@ -7,8 +7,8 @@
 
 const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbzJA2dDY7N2IY9xrwMpr-XYybw2Z8ZWybXTH8Sm7eYn1tR1qBaEAzc8N9Vp2jmM_bYVdA/exec';
 
-const TOTAL_BOXES = 24;
-const REQUIRED_BOX_IDS = Array.from({length: 24}, (_, i) => String(i + 1));
+const TOTAL_BOXES = 27;
+const REQUIRED_BOX_IDS = Array.from({length: 27}, (_, i) => String(i + 1));
 const DIVISIONS = ['Atlantic', 'Metropolitan', 'Central', 'Pacific'];
 
 async function apiGet(action, params) {
